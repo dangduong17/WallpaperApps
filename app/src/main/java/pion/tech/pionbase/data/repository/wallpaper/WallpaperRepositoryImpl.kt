513415370809451404ec97cd363df530f688fa6d
dangduong17 <dmiduong17@gmal.com>
2026-09-17 11:14:54 +0700

    fix20260916

--- app/src/main/java/pion/tech/pionbase/data/repository/wallpaper/WallpaperRepositoryImpl.kt
+++ app/src/main/java/pion/tech/pionbase/data/repository/wallpaper/WallpaperRepositoryImpl.kt
@@ -1,27 +1,24 @@
 package pion.tech.pionbase.data.repository.wallpaper
 
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.flowOn
 import pion.tech.pionbase.data.model.wallpaper.CategoryDtoModel
 import pion.tech.pionbase.data.model.wallpaper.WallpaperDtoModel
 import pion.tech.pionbase.data.remote.wallpaper.WallpaperDataSource
+import pion.tech.pionbase.data.repository.BaseRepository
 import pion.tech.pionbase.util.Result
 
 class WallpaperRepositoryImpl(
     private val dataSource: WallpaperDataSource
-) : WallpaperRepository {
-    override fun getFeaturedWallpapers(): Flow<Result<List<WallpaperDtoModel>>> = flow<Result<List<WallpaperDtoModel>>> {
-        emit(Result.Success(dataSource.getFeaturedWallpapers()))
-    }.flowOn(Dispatchers.IO).catch { emit(Result.Error(it)) }
+) : BaseRepository(), WallpaperRepository {
+    override fun getFeaturedWallpapers(): Flow<Result<List<WallpaperDtoModel>>> = executeDataCall {
+        dataSource.getFeaturedWallpapers()
+    }
 
-    override fun getTopWallpapers(): Flow<Result<List<WallpaperDtoModel>>> = flow<Result<List<WallpaperDtoModel>>> {
-        emit(Result.Success(dataSource.getTopWallpapers()))
-    }.flowOn(Dispatchers.IO).catch { emit(Result.Error(it)) }
+    override fun getTopWallpapers(): Flow<Result<List<WallpaperDtoModel>>> = executeDataCall {
+        dataSource.getTopWallpapers()
+    }
 
-    override fun getCategories(): Flow<Result<List<CategoryDtoModel>>> = flow<Result<List<CategoryDtoModel>>> {
-        emit(Result.Success(dataSource.getCategories()))
-    }.flowOn(Dispatchers.IO).catch { emit(Result.Error(it)) }
+    override fun getCategories(): Flow<Result<List<CategoryDtoModel>>> = executeDataCall {
+        dataSource.getCategories()
+    }
 }
