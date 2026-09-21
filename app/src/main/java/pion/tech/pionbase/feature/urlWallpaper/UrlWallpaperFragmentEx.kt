2bfd1f146219b5d0f7d5dae588abc0afd8fa3c82
dangduong17 <dmiduong17@gmal.com>
2026-09-21 18:40:58 +0700

    feat: improve URL wallpaper loading UX and refactor hardcoded strings

--- app/src/main/java/pion/tech/pionbase/feature/urlWallpaper/UrlWallpaperFragmentEx.kt
+++ app/src/main/java/pion/tech/pionbase/feature/urlWallpaper/UrlWallpaperFragmentEx.kt
@@ -1,6 +1,13 @@
 package pion.tech.pionbase.feature.urlWallpaper
 
+import android.graphics.Color
+import android.graphics.drawable.Drawable
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
+import com.google.android.material.snackbar.Snackbar
 import pion.tech.pionbase.R
 import pion.tech.pionbase.util.displayToast
 import pion.tech.pionbase.util.setPreventDoubleClickScaleView
